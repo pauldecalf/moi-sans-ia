5d14bd2c931862be92963d940bb5bd33f38e40c5
Serrano Paul <paul.serrano08374@gmail.com>
2023-11-23 16:17:52 +0100

    fix build and watch

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Creacid
 
 - https://github.com/Paul-Serrano/Creacid
+
+# Launch Watch
+
+- npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["index.html", "src/input.css", "js/scripts.js"],
   theme: {
     extend: {
       colors: {
