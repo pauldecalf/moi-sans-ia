8c3ee724e8db5cea43b64162ec853e73b575c6e4
Serrano Paul <paul.serrano08374@gmail.com>
2023-11-23 15:48:27 +0100

    link cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#1C5FC5",
+        red: "#E7101D",
+        green: "#0D9D57",
+        pink: "#F39ED6",
+        yellow: "#F2C107",
+        orange: "#E96404",
+      },
+    },
   },
   plugins: [],
 };
